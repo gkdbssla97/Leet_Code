19e567de360569b9ddb8d19a25868653f8c53633
Hayoon <hy97@sju.ac.kr>
2025-09-07 15:22:16 +0900

    Time: 13 ms (76.13%), Space: 49.3 MB (13.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,45 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Node> map = new LinkedHashMap<>();
+        // Map<Integer, Node> map = new LinkedHashMap<>();
+        // // LFU랑 똑같노 ㅋㅋ
+        // PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.freq, o1.freq));
+        // for(int num : nums) {
+        //     if(map.containsKey(num)) {
+        //         Node node = map.get(num);
+        //         node.freq++;
+        //         pq.remove(node);
+        //         pq.offer(node);
+        //     } else {
+        //         Node node = new Node(num, 1);
+        //         map.put(num, node);
+        //         pq.offer(node);
+        //     }
+        // }
+        // int[] res = new int[k];
+        // int idx = 0;
+        // while(k-- > 0) {
+        //     res[idx++] = pq.poll().key;
+        // }
+        // return res;
+        Map<Integer, Integer> map = new LinkedHashMap<>();
         // LFU랑 똑같노 ㅋㅋ
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.freq, o1.freq));
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.freq, o2.freq));
         for(int num : nums) {
-            if(map.containsKey(num)) {
-                Node node = map.get(num);
-                node.freq++;
-                pq.remove(node);
-                pq.offer(node);
-            } else {
-                Node node = new Node(num, 1);
-                map.put(num, node);
-                pq.offer(node);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int _k = entry.getKey();
+            int _v = entry.getValue();
+            Node node = new Node(_k, _v);
+            pq.offer(node);
+
+            if(pq.size() > k) {
+                pq.poll();
             }
         }
         int[] res = new int[k];
         int idx = 0;
-        while(k-- > 0) {
+        while(!pq.isEmpty()) {
             res[idx++] = pq.poll().key;
         }
         return res;
